793872e0cdce5d14a39fcef4012f0d4b54e96708
hajrashahid <hajrashahid92@gmail.com>
2020-06-16 09:43:52 +0500

    updated useContext to work with useState

--- src/Child.js
+++ src/Child.js
@@ -4,9 +4,11 @@ import ValueContext from './ValueContext';
 
 function Child(props) {
     let value = useContext(ValueContext);
+    let updateValue= value[1];
   return (
     <div>
-      Child number {value}
+      Child number {value[0]}
+      <button onClick = {() => {updateValue(++value[0])}}>Update Value</button>
       
     </div>
   );
