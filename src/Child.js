19c5715c336ea8b4f58e4fed0c958deba21b0baa
hajrashahid <hajrashahid92@gmail.com>
2020-06-16 08:41:30 +0500

    init repo

---
+++ src/Child.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function Child(props) {
+  return (
+    <div>
+      Child number {props.num}
+      
+    </div>
+  );
+}
+
+export default Child;
